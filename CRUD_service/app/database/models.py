def2b6f0ca0755c0247e06c2f661c1d976e3446f
Yuri Makeev <makeevyuri7@gmail.com>
2023-09-26 19:53:34 +0300

    edit models type datetime

--- CRUD_service/app/database/models.py
+++ CRUD_service/app/database/models.py
@@ -1,7 +1,6 @@
-import datetime
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON, DATETIME
+from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-
+from datetime import datetime
 from .database import Base
 
 
@@ -12,8 +11,8 @@ class Tasks(Base):
     title = Column(String, index=True, nullable=False)
     description = Column(String, index=True, nullable=False)
     is_completed = Column(Boolean, default=False, nullable=False)
-    created_at = Column(DATETIME, default=datetime.utcnow, index=True, nullable=False)
-    updated_at = Column(DATETIME, default=datetime.utcnow, index=True, nullable=False)
+    created_at = Column(datetime, default=datetime.utcnow, index=True, nullable=False)
+    updated_at = Column(datetime, default=datetime.utcnow, index=True, nullable=False)
 
 
 # Таблица Категорий
@@ -21,8 +20,8 @@ class Categories(Base):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     name = Column(String, index=True, nullable=False)
-    created_at = Column(DATETIME, default=datetime.utcnow, index=True, nullable=False)
-    updated_at = Column(DATETIME, default=datetime.utcnow, index=True, nullable=False)
+    created_at = Column(datetime, default=datetime.utcnow, index=True, nullable=False)
+    updated_at = Column(datetime, default=datetime.utcnow, index=True, nullable=False)
 
 
 # СВязь между таблицами задач и категорий
