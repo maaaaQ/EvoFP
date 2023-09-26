ace9d53d3c733b65477084ef9576d38eafd800e9
Yuri Makeev <makeevyuri7@gmail.com>
2023-09-26 19:31:57 +0300

    edit DB

--- CRUD_service/app/database/database.py
+++ CRUD_service/app/database/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "postgresql://postgres:postgresql7@127.0.0.1:5432/postgreSQL"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
